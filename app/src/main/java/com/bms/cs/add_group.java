dd8add5b839a94a9474259fdf39eebdd2e0703c5
mani9technologies <96204229+mani9technologies@users.noreply.github.com>
2022-06-17 12:02:37 +0100

    Create Group's. #5
    Incomplete

---
+++ app/src/main/java/com/bms/cs/add_group.java
@@ -0,0 +1,14 @@
+package com.bms.cs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_group extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_group);
+    }
+}
